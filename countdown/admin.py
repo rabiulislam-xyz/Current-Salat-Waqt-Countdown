671fc5980382441921958174b3bdf4a313eed173
rabiulislam <rabiulislam993@live.com>
2018-06-17 15:58:18 +0600

    added import export packages

--- countdown/admin.py
+++ countdown/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
 
 from countdown.models import Waqt
 
 
 @admin.register(Waqt)
-class WaqtAdmin(admin.ModelAdmin):
+class WaqtAdmin(ImportExportModelAdmin):
     list_filter = ('month', 'city', 'day', 'year')
--- cwc/settings.py
+++ cwc/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'countdown',
+    'import_export',
 ]
 
 MIDDLEWARE = [
